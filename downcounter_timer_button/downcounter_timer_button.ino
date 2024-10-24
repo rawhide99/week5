21936be74e8f9de628e8eca81020e18ebf395b01
Dexter <ramya.austrax239@gmail.com>
2024-10-24 09:32:19 +0530

    Button Driven Timer

---
+++ downcounter_timer_button/downcounter_timer_button.ino
@@ -0,0 +1,53 @@
+#include <avr/io.h>
+#include <util/delay.h>
+
+volatile int count = 4; // Countdown value (4 seconds)
+volatile bool counting = false;
+
+#define LED_PIN 7
+#define BUTTON_PIN 2
+
+void setup() 
+{
+    Serial.begin(9600);
+    pinMode(LED_PIN, OUTPUT);
+    digitalWrite(LED_PIN, HIGH); // Set LED to ON
+    pinMode(BUTTON_PIN, INPUT_PULLUP); // Set button pin
+
+    TCCR1A = 0;
+    TCCR1B = 0;
+    TCCR1B = (1 << WGM12); // Set CTC mode
+    OCR1A = 62499;
+    TCCR1B |= (1 << CS12);
+}
+
+void loop() 
+{
+  if (digitalRead(BUTTON_PIN) == HIGH && !counting) 
+  {
+    counting = true;
+    count = 4;
+    digitalWrite(LED_PIN, HIGH);
+    Serial.println("Timer started, counting down from 4 seconds");
+    delay(200);
+  }
+
+  if (counting) 
+  {
+    while (!(TIFR1 & (1 << OCF1A)));
+    TIFR1 |= (1 << OCF1A);
+
+    if (count > 0) 
+    {
+      Serial.print("Counting down: ");
+      Serial.println(count);
+      count--;
+    }
+    else 
+    {
+      digitalWrite(LED_PIN, LOW);
+      counting = false;
+      Serial.println("Counting finished, LED turned OFF");
+    }
+  }
+}
